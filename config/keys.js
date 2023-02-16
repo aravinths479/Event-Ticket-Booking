0d9183905f6ec479f9f7d46bccd7eb35a48be607
ARAVINTH <78143185+aravinths479@users.noreply.github.com>
2023-02-16 13:22:54 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 dbPassword = "mongodb://localhost:27017/EventTicketBooking"
-dbcloud = "mongodb+srv://eventTicketBooking:vscode1234@cluster0.st68rrl.mongodb.net/test"
+
 module.exports = {
-    mongoURI: dbcloud
+    mongoURI: dbPassword 
 };
